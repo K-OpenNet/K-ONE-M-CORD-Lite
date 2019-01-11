054516653a80ba3c36d96f630ae834021ed934a7
Woojoong Kim <woojoong.m.kim@gmail.com>
2019-01-11 22:00:48 +0900

    change network sequence

--- get_ngic_images.sh
+++ get_ngic_images.sh
@@ -8,8 +8,8 @@ docker pull woojoong/kone-dp
 echo "make networks"
 docker network create --driver=bridge --subnet=192.168.103.0/24 --ip-range=192.168.103.0/24 --gateway=192.168.103.254 brs11
 docker network create --driver=bridge --subnet=192.168.104.0/24 --ip-range=192.168.104.0/24 --gateway=192.168.104.254 brspgw
-docker network create --driver=bridge --subnet=192.168.105.0/24 --ip-range=192.168.105.0/24 --gateway=192.168.105.254 brsgi
-docker network create --driver=bridge --subnet=192.168.106.0/24 --ip-range=192.168.106.0/24 --gateway=192.168.106.254 brs1u
+docker network create --driver=bridge --subnet=192.168.105.0/24 --ip-range=192.168.105.0/24 --gateway=192.168.105.254 brs1u
+docker network create --driver=bridge --subnet=192.168.106.0/24 --ip-range=192.168.106.0/24 --gateway=192.168.106.254 brsgi
 
 echo "run Docker images"
 docker run -t -d --name cp -v $(pwd)/config:/opt/ngic/config woojoong/kone-cp:latest bash
